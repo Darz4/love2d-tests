42a8b7745e4fc83125a466689a72df9f118da16f
Darz <charles.spitzer4@gmail.com>
2018-08-10 00:34:28 +0200

    LE CARRE CHANGE DE COULEUR LOL

--- razdarz/main.lua
+++ razdarz/main.lua
@@ -1,5 +1,36 @@
 
+buildingTypes =
+{
+    {
+        name = house,
+        color = {1, 1, 1}
+    },
+    {
+        name = farm,
+        color = {0.2, 1, 0.2},
+    },
+    {
+        name = military,
+        color = {0.2, 0.2, 1}
+    }
+}
+buildingTypeIndex = 0
+entities = {}
 cellSize = 30;
+
+function love.load()
+    buildingTypeIndex = math.fmod(buildingTypeIndex + 1, #buildingTypes) + 1
+    print(buildingTypeIndex .. " size=" .. #buildingTypes)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    if isrepeat then return end
+
+    if key == "space" then
+        buildingTypeIndex = (buildingTypeIndex % #buildingTypes) + 1
+        print(buildingTypeIndex .. " size=" .. #buildingTypes)
+    end
+end
 
 function love.update()
 end
@@ -8,5 +39,7 @@ function love.draw()
     local mouseX, mouseY = love.mouse.getPosition()
     local x = math.floor(mouseX / cellSize) * cellSize
     local y = math.floor(mouseY / cellSize) * cellSize
+    local b = buildingTypes[buildingTypeIndex]
+    love.graphics.setColor(b.color[1], b.color[2], b.color[3])
     love.graphics.rectangle("fill", x, y, cellSize, cellSize)
 end
